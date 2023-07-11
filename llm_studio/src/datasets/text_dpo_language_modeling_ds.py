d8f00dc7bfdd9235738ac7073fbb8a940204a63b
Maximilian <maximilianjeblick@gmail.com>
2023-07-11 18:21:14 +0200

    simplify dataset

--- llm_studio/src/datasets/text_dpo_language_modeling_ds.py
+++ llm_studio/src/datasets/text_dpo_language_modeling_ds.py
@@ -1,4 +1,5 @@
 import logging
+from copy import copy
 from typing import Any, Dict, List, Tuple
 
 import numpy as np
@@ -53,63 +54,25 @@ class CustomDataset(LLMCustomDataset):
         assert (
             cfg.dataset.limit_chained_samples
         ), "Need to enable limit_chained_samples for dpo training"
-
+        df = df.copy()
+        df.loc[~df["chosen_response"].isna(), "output"] = df.loc[~df["chosen_response"].isna(), "chosen_response"]
         super().__init__(df=df, cfg=cfg, mode=mode)
-        self.chosen_answers = (
-            self.df[self.cfg.dataset.chosen_response_column].astype(str).values.tolist()
-        )
-        self.rejected_answer = (
-            self.df[self.cfg.dataset.rejected_response_column]
-            .astype(str)
-            .values.tolist()
-        )
+        self.chosen_answers = self.answers
+        df.loc[~df["rejected_response"].isna(), "output"] = df.loc[~df["rejected_response"].isna(), "rejected_response"]
+        super().__init__(df=df, cfg=cfg, mode=mode)
+        self.rejected_answer = self.answers
+        self.original_answers = copy(self.answers)
 
     def __getitem__(self, idx: int) -> Dict:
         """Reads a single text observation."""
+        self.answers = self.chosen_answers
         sample = super().__getitem__(idx)
+        sample_chosen = {f"chosen_{key}": value for key, value in sample.items()}
+        self.answers = self.rejected_answer
+        sample = super().__getitem__(idx)
+        sample = {f"rejected_{key}": value for key, value in sample.items()}
+        sample_chosen.update(sample)
+        sample = sample_chosen
+
         sample.pop("reward_model_prompt_text", None)
-        sample.pop("labels", None)
-
-        idx = self.indices[idx]
-
-        for prefix, text in [("chosen_", self.chosen_answers[idx]),
-                             ("rejected_", self.rejected_answer[idx])
-                             ]:
-            answer_encoding = self.encode(
-                self.tokenizer,
-                text=text,
-                max_length=self.cfg.tokenizer.max_length_answer - int(self.cfg.dataset.add_eos_token_to_answer is True),
-                truncation_side="right",
-            )["input_ids"]
-            if self.cfg.dataset.add_eos_token_to_answer:
-                answer_encoding = torch.cat(
-                    [
-                        answer_encoding,
-                        torch.Tensor([self.tokenizer.eos_token_id]),
-                    ],
-                    dim=0,
-                )
-
-            max_prompt_length = self.cfg.tokenizer.max_length - len(answer_encoding)
-            assert max_prompt_length >= 0, "Max Length Answer needs to be smaller than Max Length!"
-            prompt_input_ids = sample["input_ids"][-max_prompt_length:]
-            input_ids = torch.cat(
-                [
-                    prompt_input_ids,
-                    answer_encoding,
-                ],
-                dim=0,
-            )
-            sample[f"{prefix}_prompt_mask"] = torch.cat([torch.ones(len(prompt_input_ids)),
-                                                         torch.zeros(len(answer_encoding))])
-            sample.update(
-                self.left_pad_tokens(
-                    input_ids,
-                    attention_mask=torch.ones_like(input_ids),
-                    max_length=self.cfg.tokenizer.max_length,
-                    pad_token_id=self.tokenizer.pad_token_id,
-                    prefix=prefix
-                )
-            )
-
         return sample
